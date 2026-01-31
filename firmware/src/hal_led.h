656bb66fb26262162c36c14aa06f9c2d0c3ae7cf
TelloViz <lollisjosh@csu.fullerton.edu>
2026-01-30 16:00:59 -0800

    add better doc to hal_led.h

--- firmware/src/hal_led.h
+++ firmware/src/hal_led.h
@@ -2,8 +2,15 @@
 #pragma once
 #include <stdbool.h>
 
+/*
+hal_led.h provides an interface for initializing and controlling an
+LED on the hardware abstraction layer (HAL). It includes functions to
+intitialize the LED hardware, set the LED state (on/off), and toggle the LED. 
+*/
+
 void hal_led_init(void); // Init LED hardware
 void hal_led_set(bool on); // Set LED state
 void hal_led_toggle(void); // Toggle LED state
 
 bool hal_led_get_state(void); // Get current LED state
+
