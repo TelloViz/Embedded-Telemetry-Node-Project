f2756b3505ffaa6367805b3c94c1868c6c7429da
TelloViz <lollisjosh@csu.fullerton.edu>
2026-01-31 18:29:51 -0800

    Updated main to use new classes

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -29,24 +29,29 @@ static app::app_t g_app; // Global app state
 void setup()
 {
     Serial.begin(115200);                   // Initialize serial communication
-    hal::led::hal_led_init();               // Initialize LED hardware
-    uint32_t now = hal::time::hal_millis(); // Get current time
-    app::app_init(&g_app, now);             // Init app state
+
+    static hal::led::HalLed hLed;
+    static hal::time::HalTime hTime;
+    static hal::serial::HalSerial hSerial;
+
+    hLed.hal_led_init();               // Initialize LED hardware
+    uint32_t now = hTime.hal_millis(); // Get current time
+    app::app_init(&g_app, now, &hLed, &hTime, &hSerial);             // Init app state
     Serial.println("BOOT OK");
 }
 
 /*
-    Arduino loop function
-    - Reads serial commands and passes them to the app handler.
+        Arduino loop function
+        - Reads serial commands and passes them to the app handler.
     - Calls app_tick() to run periodic tasks.
 */
 void loop()
 {
-    uint32_t now = hal::time::hal_millis(); // Current time
+    uint32_t now = g_app.time->hal_millis(); // Current time
 
     // Read one line if available (non-blocking).
     char line[96]; // Buff for incoming command
-    if (hal::serial::serial_readline(line, sizeof(line)))
+    if (g_app.serial->serial_readline(line, sizeof(line)))
     {
         app::app_handle_command(&g_app, line); // handle command
     }
